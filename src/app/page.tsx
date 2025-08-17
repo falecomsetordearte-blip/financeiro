1844ea408b5e02943a1b906d47dc9a216d9993c9
falecomsetordearte-blip <falecomsetordearte@gmail.com>
2025-08-17 11:41:15 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Meu Controle Financeiro</h1>
+      <p>Aqui vamos adicionar o formulário para lançar as contas.</p>
+    </main>
+  );
+}
